536861bd88b49076a36110d6f2815b927a38a45b
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-09-16 21:48:34 -0400

    tools split from parts

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,5 @@
 from app import app
-from models import db, User, Project, Task, Part
+from models import db, User, Project, Task, Part, Tool
 
 with app.app_context():
 
@@ -16,12 +16,19 @@ with app.app_context():
     demo_task=Task(name="Replace Seals",description="Replace weather strips",project=demo_project)
     demo_project.tasks.append(demo_task)
 
-    demo_part=Part(name="Weather Strip Kit",part_type="Consumable",amount_required=1,amount_owned=0,source="DeLorean Midwest", cost=200, user=demo_user)
+    demo_part=Part(name="Weather Strip Kit",amount_required=1,amount_owned=0,source="DeLorean Midwest", cost=200, user=demo_user)
     demo_user.parts.append(demo_part)
     demo_task.parts.append(demo_part)
+
+    demo_tools=[
+        Tool(name='Wrench 10mm',owned=True, user=demo_user),
+        Tool(name='Wrench 13mm', owned=True, user=demo_user),
+        Tool(name="Rivet Gun",owned=False,cost=70, user=demo_user)
+    ]
 
     db.session.add(demo_user)
     db.session.add(demo_project)
     db.session.add(demo_task)
     db.session.add(demo_part)
+    db.session.add_all(demo_tools)
     db.session.commit()
