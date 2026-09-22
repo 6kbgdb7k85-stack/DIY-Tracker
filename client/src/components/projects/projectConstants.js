b528098ae8f98ec334003d9a7a3194f98050ce18
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-09-22 11:57:41 -0400

    task crud complete

--- client/src/components/projects/projectConstants.js
+++ client/src/components/projects/projectConstants.js
@@ -7,9 +7,10 @@ export const PROJECT_TABLE_COLUMNS = [
 ];
 
 export const PROJECT_TASKS_COLUMNS = [
-  { id: "completed", label: "Completed", type:FieldTypes.CHECKBOX},
+  { id: "completed", label: "Completed", type: FieldTypes.CHECKBOX },
   { id: "name", label: "Name" },
   { id: "description", label: "Description" },
+  { id: "time", label: "Estimated Time to Complete" },
 ];
 
 export const PROJECT_TASKS_SUBTABLE = [
@@ -24,10 +25,10 @@ export const PROJECT_TOOL_COLUMNS = [
   { id: "cost", label: "Cost to Buy", type: FieldTypes.NUMBER },
 ];
 
-export const PROJECT_PART_COLUMNS=[
-    {id:'name',label:'Name'},
-    {id:'amount_required',label:'Units Required', type: FieldTypes.NUMBER},
-    {id:'amount_owned',label:'Units Already Owned', type: FieldTypes.NUMBER},
-    {id:'cost',label:'Cost per Unit', type: FieldTypes.NUMBER},
-    {id:'source',label:'Source'}
-]
+export const PROJECT_PART_COLUMNS = [
+  { id: "name", label: "Name" },
+  { id: "amount_required", label: "Units Required", type: FieldTypes.NUMBER },
+  { id: "amount_owned", label: "Units Already Owned", type: FieldTypes.NUMBER },
+  { id: "cost", label: "Cost per Unit", type: FieldTypes.NUMBER },
+  { id: "source", label: "Source" },
+];
